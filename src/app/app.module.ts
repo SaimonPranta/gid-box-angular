521e4516f000563d4768a767274386816c737a7c
pranta <saimonpranta@gmail.com>
2022-12-01 00:03:42 +0600

    After fixed some issu in container margin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
-import { TomeComponent } from './pages/tome/tome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    TomeComponent
   ],
   imports: [
     BrowserModule,
